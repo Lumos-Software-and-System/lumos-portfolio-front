71e9a576d121d3f2070ef373018d127dde4ec731
RichardLimaDxD <richardpires663802@gmail.com>
2024-03-21 17:36:42 -0300

    feat:create configs

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import theme from "@/utils/styles/theme";
+import { ThemeProvider } from "@mui/material";
+import type { Metadata } from "next";
+import "@/scss/global.scss";
+
+export const metadata: Metadata = {
+  title: "Lumos",
+  description: "Lumos",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="pt-br">
+      <body>
+        <ThemeProvider theme={theme}>{children}+ </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
