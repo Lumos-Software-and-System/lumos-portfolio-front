3595311775e802e09fbddba07aa080201264d520
RichardLimaDxD <richardpires663802@gmail.com>
2024-03-21 21:59:57 -0300

    feat:merge header and section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/headers";
+import SectionWorks from "@/components/section/section-works";
 
 const Home = () => {
   return (
-    <main>
+    <>
       <Header />
-    </main>
+      <main>
+        <SectionWorks />
+      </main>
+    </>
   );
 };
 
