12176dfdfe040bc9478111b7332800b60d6bdaf8
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-03-21 23:27:36 -0300

    feat: create benefits section and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Header from "@/components/headers";
+import SectionBenefits from "@/components/section/section-benefits";
 import SectionWorks from "@/components/section/section-works";
 
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <SectionWorks />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <SectionWorks />
+                <SectionBenefits />
+            </main>
+        </>
+    );
 };
 
 export default Home;
---
+++ src/data/benefits.ts
@@ -0,0 +1,31 @@
+import { Benefit } from "@/interfaces/benefitsData.interface";
+import dominioImagem from "../assets/globe-05.svg";
+
+const benefitsData: Array<Benefit> = [
+    {
+        image: "/globe-05.svg",
+        title: "Domínio Grátis",
+        description:
+            "A Lumos oferece domínio para o seu site de forma gratuita, para que sua empresa possa economizar muito mais!",
+    },
+    {
+        image: "/message-chat-01.svg",
+        title: "Suporte Especializado",
+        description:
+            "O nosso suporte está sempre pronto para solucionar seus problemas e sanar suas dúvidas. Conte conosco sempre que precisar de ajuda!",
+    },
+    {
+        image: "/data-01.svg",
+        title: "Personalização do seu sistema",
+        description:
+            "Possuimos sistemas personalizáveis para suprir todas as necessidades do seu negócio!",
+    },
+    {
+        image: "/clock-plus.svg",
+        title: "Entrega rápida",
+        description:
+            "Nosso foco é realizar a entrega do seu sistema da forma mais rápida possível!",
+    },
+];
+
+export default benefitsData;
