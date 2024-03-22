038008944c2a0f3725b14093c09ea9efa7879db5
RichardLimaDxD <richardpires663802@gmail.com>
2024-03-21 21:48:55 -0300

    feat: create works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import SectionWorks from "@/components/section/section-works";
+
 const Home = () => {
   return (
-    <main>
-      <h2>aqaejdaskjlhdasjkld</h2>
-    </main>
+    <>
+      <main>
+        <SectionWorks />
+      </main>
+    </>
   );
 };
 
