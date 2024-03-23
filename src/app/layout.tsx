a1df0e1acd66694dd23657d9a7df4b14dcaddf8a
RichardLimaDxD <richardpires663802@gmail.com>
2024-03-22 22:16:32 -0300

    feat: update form component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import theme from "@/utils/styles/theme";
-import { ThemeProvider } from "@mui/material";
 import type { Metadata } from "next";
 import ContextProvider from "@/contexts/contexts";
 
 import "@/scss/global.scss";
 
 export const metadata: Metadata = {
-    title: "Lumos",
-    description: "Lumos",
+  title: "Lumos",
+  description: "Lumos",
 };
 
 const RootLayout = ({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) => {
-    return (
-        <html lang="pt-br">
-            <body>
-                <ContextProvider>
-                    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-                </ContextProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pt-br">
+      <body>
+        <ContextProvider>{children}</ContextProvider>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
