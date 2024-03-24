4e79d6e30c44a46d086e24ba91defc079a40bbcb
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-03-24 20:38:35 -0300

    style: change some of the form styles

--- src/components/forms/subscrib-form/index.tsx
+++ src/components/forms/subscrib-form/index.tsx
@@ -7,50 +7,50 @@ import useSubscribe from "@/hooks/useSubscribe.hook";
 import styles from "./styles.module.scss";
 
 const SubscribForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Subscribs>({
-    resolver: zodResolver(subscribSchema),
-  });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Subscribs>({
+        resolver: zodResolver(subscribSchema),
+    });
 
-  const { postSubscribe } = useSubscribe();
+    const { postSubscribe } = useSubscribe();
 
-  const submit = (formData: Subscribs) => {
-    postSubscribe(formData);
-  };
+    const submit = (formData: Subscribs) => {
+        postSubscribe(formData);
+    };
 
-  return (
-    <form
-      className={styles.container__formSubscrib}
-      onSubmit={handleSubmit(submit)}
-    >
-      <h2>Fique por dentro das novidades!</h2>
+    return (
+        <form
+            className={styles.container__formSubscrib}
+            onSubmit={handleSubmit(submit)}
+        >
+            <h2>Fique por dentro das novidades!</h2>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Digite seu nome..."
-          {...register("name")}
-        />
-        {errors.name?.message && <p> * {errors.name?.message}</p>}
-      </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Digite seu nome..."
+                    {...register("name")}
+                />
+                {errors.name?.message && <p> * {errors.name?.message}</p>}
+            </div>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Digite seu E-mail..."
-          {...register("email")}
-        />
-        {errors.email?.message && <p> * {errors.email?.message}</p>}
-      </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Digite seu e-mail..."
+                    {...register("email")}
+                />
+                {errors.email?.message && <p> * {errors.email?.message}</p>}
+            </div>
 
-      <button>
-        <span>Enviar email</span>
-      </button>
-    </form>
-  );
+            <button>
+                <span>Enviar email</span>
+            </button>
+        </form>
+    );
 };
 
 export default SubscribForm;
