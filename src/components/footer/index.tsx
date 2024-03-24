be91c9e971321e3f2c8189b1864f62a3866b9afd
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-03-24 20:50:45 -0300

    style: change some footer styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,15 +2,15 @@ import SocialMedias from "../social-medias";
 import styles from "./styles.module.scss";
 
 const Footer = () => {
-  return (
-    <footer className={styles.container__footer}>
-      <h2>Logo</h2>
-      <div>
-        <span>© 2024 Lumos Software and System.</span>
-        <SocialMedias />
-      </div>
-    </footer>
-  );
+    return (
+        <footer className={styles.container__footer}>
+            <h2>Logo</h2>
+            <div>
+                <SocialMedias />
+                <span>© 2024 Lumos Software and System.</span>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
