7bca7a9a51e52fe0574b486c48fbf0ea1f626be7
RichardLimaDxD <richardpires663802@gmail.com>
2024-03-25 15:51:28 -0300

    feat:create animations

--- src/components/section/section-works/index.tsx
+++ src/components/section/section-works/index.tsx
@@ -4,36 +4,32 @@ import rectangle from "../../../assets/Rectangle55.svg";
 import styles from "./styles.module.scss";
 
 const SectionWorks = () => {
-    return (
-        <>
-            <section className={styles.container__sectionWorks}>
-                <div>
-                    <h1>
-                        Lumos construindo o caminho para o sucesso digital da
-                        sua marca.
-                    </h1>
-                    <p>
-                        Da criação de páginas simples até sistemas mais
-                        complexos, fornecemos serviços e trazemos seu negócio
-                        para o digital de forma personalizada!
-                    </p>
-                    <button>Entre em contato</button>
-                </div>
-                <Image
-                    className={styles["vector--img"]}
-                    priority={true}
-                    src={vector}
-                    alt="background img"
-                />
-                <Image
-                    className={styles["rectangle--img"]}
-                    priority={true}
-                    src={rectangle}
-                    alt="background img"
-                />
-            </section>
-        </>
-    );
+  return (
+    <section className={styles.container__sectionWorks}>
+      <div>
+        <h1>
+          Lumos construindo o caminho para o sucesso digital da sua marca.
+        </h1>
+        <p>
+          Da criação de páginas simples até sistemas mais complexos, fornecemos
+          serviços e trazemos seu negócio para o digital de forma personalizada!
+        </p>
+        <button>Entre em contato</button>
+      </div>
+      <Image
+        className={styles["vector--img"]}
+        priority={true}
+        src={vector}
+        alt="background img"
+      />
+      <Image
+        className={styles["rectangle--img"]}
+        priority={true}
+        src={rectangle}
+        alt="background img"
+      />
+    </section>
+  );
 };
 
 export default SectionWorks;
